67ae2c4cbb67fb5a159ce736d561115a542dc573
natalie palacios <natalie.pm82@outlook.com>
2022-10-24 18:10:24 -0700

    edit handle cancel event
    deleted preventDefault(), reload(), and return(). An event does not need a preventDefault or a return value

--- front-end/src/dashboard/ReservationList.js
+++ front-end/src/dashboard/ReservationList.js
@@ -1,51 +1,59 @@
 import React from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { updateReservationStatus } from "../utils/api";
 
 function ReservationList({ reservation }) {
+    const {
+      reservation_id,
+      first_name,
+      last_name,
+      mobile_number,
+      reservation_time,
+      people,
+      status,
+    } = reservation;
+
     const history = useHistory();
     
     const handleCancel = (event) => {
-      event.preventDefault();
       const userResponse = window.confirm("Do you want to cancel this reservation? This cannot be undone.");
       if (userResponse) {
         const abortController = new AbortController();
         async function update() {
           try {
-            await updateReservationStatus("cancelled", reservation.reservation_id, abortController.signal);
+            console.log("reservation id: ", reservation_id);
+            await updateReservationStatus("cancelled", reservation_id, abortController.signal);
             history.push("/");
-            window.location.reload();
           } catch (error) {
             console.log(error);
           }
         }
   
         update();
-        return () => abortController.abort();
       }
     };
     
     return (
       <div className="card col-sm-8 col-md-7 col-lg-6 m-auto">
         <div className="card-body">
-          <h5 className="card-title ">{reservation.first_name} {reservation.last_name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Phone Number: {reservation.mobile_number}</h6>
-          <p className="card-text">Time: {reservation.reservation_time}</p>
+          <h5 className="card-title ">{first_name} {last_name}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Phone Number: {mobile_number}</h6>
+          <p className="card-text">Time: {reservation_time}</p>
           <p className="card-text">
-            Party Size: {reservation.people}
+            Party Size: {people}
           </p>
-          <p data-reservation-id-status={reservation.reservation_id}>
-            Status: {reservation.status}
+          <p data-reservation-id-status={reservation_id}>
+            Status: {status}
           </p>
-          {reservation.status === "booked" &&
+          {status === "booked" &&
             <>
-              <a className="btn btn-outline-info" href={`/reservations/${reservation.reservation_id}/seat`}>
+              <a className="btn btn-outline-info" href={`/reservations/${reservation_id}/seat`}>
                 Seat
               </a>
-              <Link className="btn btn-outline-warning mx-2" to={`/reservations/${reservation.reservation_id}/edit`}>
+              <a className="btn btn-outline-warning mx-2" href={`/reservations/${reservation_id}/edit`}>
                 Edit
-              </Link>
-              <button className="btn btn-outline-secondary" onClick={handleCancel} data-reservation-id-cancel={reservation.reservation_id}>
+              </a>
+              <button className="btn btn-outline-secondary" onClick={handleCancel} data-reservation-id-cancel={reservation_id}>
                 Cancel
               </button>
             </>
